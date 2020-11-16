2beacdf947be766d12536f8aa09ff8034601e558
santeive <packo.ive@gmail.com>
2020-11-15 21:35:48 -0600

    Estado del componente y actualización del estado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Fragment} from 'react'
 import "./styles/styles.scss";
 import Curso from "./Curso"
+import Banner from "./Banner"
+import Formulario from "./Formulario"
 
 const cursos = [
     {
@@ -14,44 +16,15 @@ const cursos = [
         "image": "https://cdn.vox-cdn.com/thumbor/RVhobHgNC4xy7NSqXvVs13OdJIk=/0x0:4000x4000/1200x800/filters:focal(1680x1680:2320x2320)/cdn.vox-cdn.com/uploads/chorus_image/image/55649931/IM_Photo5.0.jpg",
         "price": "300",
         "teacher": "Carlos Garcia"
-    },
-    {
-        "title": "Spark desde cero",
-        "image": "https://cdn.vox-cdn.com/thumbor/RVhobHgNC4xy7NSqXvVs13OdJIk=/0x0:4000x4000/1200x800/filters:focal(1680x1680:2320x2320)/cdn.vox-cdn.com/uploads/chorus_image/image/55649931/IM_Photo5.0.jpg",
-        "price": "30",
-        "teacher": "Roberto Garcia"
     }
 ]
 
 const App = () => (
     <Fragment>
-        <div className="main-banner img-container l-section" id="main-banner">
-            <div className="ed-grid lg-grid-6">
-                <div className="lg-cols-4 lg-x-2">
-                <img className="main-banner__img" alt="background-img" src="https://wallpapercave.com/wp/wp1887286.jpg"/>
-                <div className="main-banner__data s-center">
-                    <p className="t2 s-mb-0">Curso de ED Team</p>
-                    <p> Tu futuro te está esperando</p>
-                    <a href="https://ed.team/" className="button">Suscribirse</a>
-                </div>
-                </div>
-            </div>
-        </div>
+        <Banner />
 
+        {/* Grid de cursos */}
         <div className="ed-grid m-grid-3">
-            <Curso 
-                title="React desde cero con Packo" 
-                image="https://edteam-media.s3.amazonaws.com/courses/medium/0b4e6141-c9cd-4e05-bb59-55b4b9e67a14.png" 
-                price="50 USD"
-                teacher="Francisco Garcia"
-            />
-            <Curso 
-                title="Python desde cero con Sante " 
-                image="https://edteam-media.s3.amazonaws.com/courses/big/671fe4c0-bbac-4bbe-8a0e-3fb0811a8353.png" 
-                price="80 USD"
-                teacher="Francisco Enriquez"
-            />
-
             {/* Para un card con valores por default */}
             <Curso />
 
@@ -59,8 +32,10 @@ const App = () => (
             {
                 cursos.map( c => <Curso title={c.title} image={c.image} price={c.price} teacher={c.teacher} /> )
             }
-            
+
         </div>
+
+        <Formulario />
     </Fragment>
 )
 
